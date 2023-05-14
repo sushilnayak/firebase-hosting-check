99df9db0134b11f3d805b6faa50602b6c72ed72e
Sushil Nayak <sushil.nayak@gmail.com>
2023-05-14 17:18:51 +0530

    intial

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Testing Firebase Hosting Check!!!
         </p>
+        <p>${process.env.NAME}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
